a44bb953d2d4243050bc42afa08868478d67f730
Rene <racosta323@gmail.com>
2024-02-07 11:57:41 -0600

    set stock

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { useState } from "react"
 
 function PlantCard({ name, image, price }) {
+
+  const[stock, setStock]= useState(true)
 
   return (
     <li className="card" data-testid="plant-item">
       <img src={ image } alt={ name } />
       <h4>{ name }</h4>
       <p>Price: { price }</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {stock ? (
+        <button 
+          className="primary"
+          onClick={()=>{setStock(false)}}
+        > 
+          In Stock
+        </button>
       ) : (
         <button>Out of Stock</button>
       )}
